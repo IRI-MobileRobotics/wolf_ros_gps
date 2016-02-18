08b0db6af19c946f590c49cea609f0d7f248281d
Pier Tirindelli <pier.tirindelli@gmail.com>
2016-02-18 12:30:06 +0100

    first commit, adding odometry from scratch

--- include/wolf_gps_node.h
+++ include/wolf_gps_node.h
@@ -15,6 +15,8 @@
 #include "wolf/wolf.h"
 #include "wolf/wolf_problem.h"
 #include "wolf/capture_motion.h"
+#include "wolf/capture_odom_2D.h"
+#include <wolf/capture_fix.h>
 
 /**************************
  *     CERES includes     *
@@ -44,79 +46,25 @@
 class WolfGPSNode
 {
 public:
-    WolfGPSNode(SensorGPS* _gps_sensor_ptr,
-                const FrameStructure _frame_structure,
-                SensorBase* _sensor_prior_ptr,
-                const Eigen::VectorXs& _prior,
-                const Eigen::MatrixXs& _prior_cov,
-                const unsigned int& _trajectory_size = 10,
-                const WolfScalar& _new_frame_elapsed_time = 0.1);
+    WolfGPSNode();
     virtual ~WolfGPSNode();
 
-    void createFrame(const Eigen::VectorXs& _frame_state, const TimeStamp& _time_stamp);
-    void createFrame(const TimeStamp& _time_stamp);
-    void manageWindow();
-
-    void obsCallback(const iri_common_drivers_msgs::SatellitePseudorangeArray::ConstPtr& msg);
 
 protected:
-    void initCeresManager();
-    void updateWolfProblem();
-    bool checkNewFrame(CaptureBase* new_capture);
-
-protected:
-    //Debug stuff
-    bool ceresVerbose = false;
-    ros::Time time_last_obs_;
-    ros::Duration down_sampling_interval_ = ros::Duration(0, 5e8);
-
-    /*
-     * WOLF stuff
-     */
-
-    // GPS sensor
-    SensorGPS* gps_sensor_ptr_;
-    // Odometry sensor
-    SensorOdom2D* odom_sensor_ptr_;
-
-    // Wolf Problem
-    WolfProblem* problem_;
-    FrameStructure frame_structure_;
-
-    //pointer to a sensor providing predictions
-    SensorBase* sensor_prior_;
-
-    //auxiliar/temporary iterators, frames and captures
-    FrameBaseIter first_window_frame_;
-    FrameBase* current_frame_;
-    FrameBase* last_key_frame_;
-    CaptureMotion* last_capture_relative_;
-    CaptureMotion* second_last_capture_relative_;
-    std::queue<CaptureBase*> new_captures_;
-
-    //Manager parameters
-    unsigned int trajectory_size_;
-    WolfScalar new_frame_elapsed_time_;
-
-    // Ceres
-    ceres::Solver::Options ceres_options_;
-    ceres::Problem::Options problem_options_;
-    CeresManager* ceres_manager_;
 
     /*
      * ROS stuff
      */
     // Subscribers
-    ros::Subscriber obs_sub_; // obs (measurements) subscriber
-    // Publishers
-    ros::Publisher wolf_fix_pub_;
+    ros::Subscriber gps_sub_; // obs (measurements) subscriber
+    ros::Subscriber odom_sub_; // obs (measurements) subscriber
+
     // ROS node handle
     ros::NodeHandle nh_;
 
     //Odometry callback
-    ros::Time last_odom_stamp_;
-    ros::Subscriber odometry_subscriber_;
-    void odometry_callback(const nav_msgs::Odometry::ConstPtr& msg);
+    void gpsCallback(const iri_common_drivers_msgs::SatellitePseudorangeArray::ConstPtr& msg);
+    void odometryCallback(const nav_msgs::Odometry::ConstPtr& msg);
 
 };
 
--- src/wolf_gps_main.cpp
+++ src/wolf_gps_main.cpp
@@ -21,25 +21,23 @@ int main(int argc, char **argv)
     sensor_o->fix(); //orientation is fixed, because antenna omnidirectional, so is not going to be optimized
     StateBlock* sensor_bias = new StateBlock(Eigen::Vector1s::Zero());    //gps sensor bias
 
-    StateBlock* init_vehicle_p = new StateBlock(Eigen::Vector3s::Zero());    //vehicle initial position
+    StateBlock* init_vehicle_p = new StateBlock(Eigen::Vector3s(4789000, 177000, 4195000));    //vehicle initial position
     StateBlock* init_vehicle_o = new StateBlock(Eigen::Vector1s::Zero());// vehicle initial orientation
 
     SensorGPS* gps_sensor_ptr = new SensorGPS(sensor_p, sensor_o, sensor_bias, init_vehicle_p, init_vehicle_o);
 
-    Eigen::Vector3s prior = Eigen::Vector3s::Zero();
-    prior[0] = 4789360;
-    prior[1] = 177175;
-    prior[2] = 4194534;
+    Eigen::Vector3s prior = Eigen::Vector3s(10, 10, 90*M_PI/180);
+
+    // init wolf odom sensor
+    WolfScalar odom_std[2];
+    odom_std[0] = 0.2; // odometry_translational_std
+    odom_std[1] = 0.2; // odometry_rotational_std
+    SensorOdom2D* odom_sensor_ptr_ = new SensorOdom2D(new StateBlock(Eigen::Vector2s::Zero()), new StateBlock(Eigen::Vector1s::Zero()), odom_std[0], odom_std[1]);//both arguments initialized on top
 
 
-            // Trilateration node
-    WolfGPSNode wgNode(gps_sensor_ptr,
-                       PO_2D,
-                       nullptr,                           //_sensor_prior_ptr
-                       Eigen::Vector3s::Zero(),           //prior
-                       Eigen::Matrix3s::Identity()*0.01,  //prior cov
-                       5,                                 //window size
-                       1);                                //time for new keyframe
+
+    // Trilateration node
+    WolfGPSNode wgNode();
 
     ros::spin();
 
