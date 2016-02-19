dfeb67cbf79f6d96808cfe1ef0043bfaba733f5e
Pier Tirindelli <pier.tirindelli@gmail.com>
2016-02-19 16:58:55 +0100

    copied and merged code from wolf_manager and wolf_alg_node. is compiling but not working

--- include/wolf_gps_node.h
+++ include/wolf_gps_node.h
@@ -32,6 +32,8 @@
  *      ROS includes      *
  **************************/
 #include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
 
 #include "iri_common_drivers_msgs/SatellitePseudorangeArray.h"
 #include "iri_common_drivers_msgs/NavSatFix_ecef.h"
@@ -46,29 +48,94 @@
 class WolfGPSNode
 {
 public:
-    WolfGPSNode();
+    WolfGPSNode(const FrameStructure _frame_structure,
+                SensorBase* _sensor_prior_ptr,
+                const Eigen::VectorXs& _prior,
+                const Eigen::MatrixXs& _prior_cov,
+                const unsigned int& _trajectory_size,
+                const WolfScalar& _new_frame_elapsed_time);
     virtual ~WolfGPSNode();
 
     bool hasDataToProcess();
     void process();
 
-protected:
+    //wolf manager methods
+    void createFrame(const Eigen::VectorXs& _frame_state, const TimeStamp& _time_stamp);
+    void createFrame(const TimeStamp& _time_stamp);
+    void manageWindow();
+    void addSensor(SensorBase* _sensor_ptr);
 
-    /*
-     * ROS stuff
-     */
-    // Subscribers
-    ros::Subscriber gps_sub_; // obs (measurements) subscriber
-    ros::Subscriber odom_sub_; // obs (measurements) subscriber
+protected:
+    //-------------- WOLF manager stuff--------------
+    //sets the problem
+    WolfProblem* problem_;
+    FrameStructure frame_structure_;
+
+    //pointer to a sensor providing predictions
+    SensorBase* sensor_prior_;
+
+    //auxiliar/temporary iterators, frames and captures
+    FrameBaseIter first_window_frame_;
+    FrameBase* current_frame_;
+    FrameBase* last_key_frame_;
+    CaptureMotion* last_capture_relative_;
+    CaptureMotion* second_last_capture_relative_;
+    std::queue<CaptureBase*> new_captures_;
+
+    //Manager parameters
+    unsigned int trajectory_size_;
+    WolfScalar new_frame_elapsed_time_;
+
+    void addCapture(CaptureBase* _capture);
+    void update();
+    bool checkNewFrame(CaptureBase* new_capture);
+    Eigen::VectorXs getVehiclePose(const TimeStamp& _now = 0);
+
+    //---------------WOLF NODE STUFF-----------------
+    //transforms
+    tf::TransformBroadcaster tfb_;
+    tf::TransformListener    tfl_;
+    tf::Transform Tf_map2base_; //wolf output
+    tf::Transform Tf_odom2base_; //published by odom source
+    tf::Transform Tf_map2odom_; //to be broadcasted by this node
+    std::string base_frame_name_;
+    std::string gps_frame_name_;
+
+    std::string ecef_frame_name_;
+    std::string map_frame_name_;
+    std::string odom_frame_name_;
+
+    void loadGPSTf();
+    bool gps_tf_loaded_;
+
+    //ceres
+    int max_iterations_;
+    bool use_auto_diff_wrapper_, apply_loss_function_;
+    ceres::Solver::Options ceres_options_;
+    ceres::Problem::Options problem_options_;
+    CeresManager* ceres_manager_;
+    //-----------------------------------------------
+
+
+//    E' il sensor prior!
+//    //Wolf: odom sensor
+//    SensorOdom2D* odom_sensor_ptr_;
+
+    //Wolf: gps sensor
+    SensorGPS* gps_sensor_ptr_;
+
+    //Odometry callback
+    ros::Time last_odom_stamp_;
+    ros::Subscriber odom_sub_; // odometry subscriber
+    void odometryCallback(const nav_msgs::Odometry::ConstPtr& msg);
+
+    // GPS callback
+    ros::Subscriber gps_sub_; // gps subscriber
+    void gpsCallback(const iri_common_drivers_msgs::SatellitePseudorangeArray::ConstPtr& msg);
+    int gps_data_arrived_;
 
     // ROS node handle
     ros::NodeHandle nh_;
-
-    //Odometry callback
-    void gpsCallback(const iri_common_drivers_msgs::SatellitePseudorangeArray::ConstPtr& msg);
-    void odometryCallback(const nav_msgs::Odometry::ConstPtr& msg);
-    int gps_data_arrived;
-
 
 };
 
--- src/wolf_gps_main.cpp
+++ src/wolf_gps_main.cpp
@@ -9,35 +9,47 @@ int main(int argc, char **argv)
 {
     std::cout << "\n=========== WOLF GPS MAIN ===========\n\n";
 
-    //init ros
+    /*
+     * Init ROS
+     */
     ros::init(argc, argv, "wolf_gps_node");
 
     /*
-     * Parameters, to be optimized
+     * Init GPS sensor
      */
     StateBlock* sensor_p = new StateBlock(Eigen::Vector3s::Zero()); //gps sensor position
     sensor_p->fix(); // TODO only for now, to simplify things
     StateBlock* sensor_o = new StateBlock(Eigen::Vector4s::Zero(), ST_QUATERNION);   //gps sensor orientation
     sensor_o->fix(); //orientation is fixed, because antenna omnidirectional, so is not going to be optimized
     StateBlock* sensor_bias = new StateBlock(Eigen::Vector1s::Zero());    //gps sensor bias
-
     StateBlock* init_vehicle_p = new StateBlock(Eigen::Vector3s(4789000, 177000, 4195000));    //vehicle initial position
     StateBlock* init_vehicle_o = new StateBlock(Eigen::Vector1s::Zero());// vehicle initial orientation
-
     SensorGPS* gps_sensor_ptr = new SensorGPS(sensor_p, sensor_o, sensor_bias, init_vehicle_p, init_vehicle_o);
 
-    Eigen::Vector3s prior = Eigen::Vector3s(10, 10, 90*M_PI/180);
-
-    // init wolf odom sensor
+    /*
+     * Init odometry sensor
+     */
     WolfScalar odom_std[2];
     odom_std[0] = 0.2; // odometry_translational_std
     odom_std[1] = 0.2; // odometry_rotational_std
     SensorOdom2D* odom_sensor_ptr_ = new SensorOdom2D(new StateBlock(Eigen::Vector2s::Zero()), new StateBlock(Eigen::Vector1s::Zero()), odom_std[0], odom_std[1]);//both arguments initialized on top
 
+    /*
+     * Init prior
+     */
+    Eigen::Vector3s prior = Eigen::Vector3s(10, 10, 90*M_PI/180);
 
+    /*
+     * params for wolf manager etc
+     */
+    int window_length_ = 8;
+    double new_frame_elapsed_time_ = 1.0;
 
-    // Trilateration node
-    WolfGPSNode* wgps = new WolfGPSNode();
+    /*
+     * Wolf ROS node
+     */
+    WolfGPSNode* wgps = new WolfGPSNode(PO_2D, odom_sensor_ptr_,Eigen::Vector3s::Zero(), Eigen::Matrix3s::Identity()*0.01, window_length_, new_frame_elapsed_time_);
+
 
 
     ros::Rate loopRate(2);// TODO wgps().getRate());
